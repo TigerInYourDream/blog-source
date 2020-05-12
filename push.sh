d8fc6c65a43239a0fa0d61bfe916dcd3cd83f63e
TigerInYourDream <zyzzz0928@gmail.com>
2020-05-12 11:53:21 +0800

    upd |> commit soure to github
    Signed-off-by: TigerInYourDream <zyzzz0928@gmail.com>

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 echo "push blog source to github"
 git pull
 git add .
-git commit -s -m "commit sour to github"
+git commit -s -m "upd |> commit soure to github"
 git push origin master
 echo "push blog source to github end"
